93337560d9c2d37273d95d3c9e44b8d392dd85fc
Johnny <ajay@maaxframe.com>
2016-06-30 17:23:34 -0400

    Test run of Simple Macd - still wip

--- Strategy/SimplyMACD.c
+++ Strategy/SimplyMACD.c
@@ -1,11 +1,16 @@
 // by Ajay Malhotra
-
+// A simple MACD indicator going by crossOver
+// The problem seems to be fundamental as crossover
+// Using a moving average will keep moving till the bar is closed
+// Thereby using 1 minute as m1 data is supplied
+// but multiplying the periods by 60
 //#define USE_PREDICT
 
 function run()
 {
 	StartDate = 2012;
-	BarPeriod	= 60;
+	BarPeriod	= 1;
+  LookBack = 60;
 
 	while (asset(loop("EUR/USD"))) {
 	var FastPeriod, SlowPeriod, SignalPeriod;
